b915538fb7965754fa03fb3e752e4ed2dddce74e
uonod1323 <uonod1323@naver.com>
2022-11-28 17:35:02 +0900

    감찾기1

--- vuedongsan2/src/App.vue
+++ vuedongsan2/src/App.vue
@@ -1,15 +1,35 @@
 <template>
-  <img alt="Vue logo" src="./assets/logo.png">
-  <HelloWorld msg="Welcome to Your Vue.js App"/>
+<div class="menu">
+  <div class="menu">
+    <a v-for="(작명,i) in 메뉴들[0]" :key="i">{{작명}}</a>
+  </div>
+</div>
+<div>
+  <h4>{{products[0]}}</h4>
+  <p>50만원</p>
+</div>
+<div>
+  <h4>{{products[1]}}</h4>
+  <p>50만원</p>
+</div>
+<div>
+  <h4>{{products[2]}}</h4>
+  <p>50만원</p>
+</div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
 
 export default {
-  name: 'App',
-  components: {
-    HelloWorld
+  name : 'App',
+  data(){
+    return {
+      products : ['역삼동원룸', '천호동원룸', '마포구원룸'],
+      메뉴들 : [{menu1 : 'Home', menu2 : 'Shop', menu3 : 'About'}, {menu1 : 'what', menu2 : 'the', menu3 : 'fuck'}],
+      price1 : 60,
+      price2 : 50,
+      price3 : 40,
+    }
   }
 }
 </script>
@@ -22,5 +42,14 @@ export default {
   text-align: center;
   color: #2c3e50;
   margin-top: 60px;
+}
+.menu {
+  background : darkslateblue;
+  padding : 15px;
+  border-radius : 5px;
+}
+.menu a {
+  color : white;
+  padding : 10px;
 }
 </style>
