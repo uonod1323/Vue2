5edfd822b5fac9798bce6c714b17af3a9f7169a9
uonod1323 <uonod1323@naver.com>
2022-11-28 21:50:16 +0900

    for문 실험

--- vuedongsan2/src/App.vue
+++ vuedongsan2/src/App.vue
@@ -1,20 +1,12 @@
 <template>
 <div class="menu">
   <div class="menu">
-    <a v-for="(작명,i) in 메뉴들[0]" :key="i">{{작명}}</a>
+    <a v-for="작명 in 메뉴들" :key="작명">{{작명}}</a>
   </div>
 </div>
-<div>
-  <h4>{{products[0]}}</h4>
-  <p>50만원</p>
-</div>
-<div>
-  <h4>{{products[1]}}</h4>
-  <p>50만원</p>
-</div>
-<div>
-  <h4>{{products[2]}}</h4>
-  <p>50만원</p>
+<div v-for="(작명, i) in productsDetail" :key="i">
+  <h4>{{productsDetail[i].products}}</h4>
+  <p>{{productsDetail[i].price}}</p>
 </div>
 </template>
 
@@ -25,10 +17,13 @@ export default {
   data(){
     return {
       products : ['역삼동원룸', '천호동원룸', '마포구원룸'],
-      메뉴들 : [{menu1 : 'Home', menu2 : 'Shop', menu3 : 'About'}, {menu1 : 'what', menu2 : 'the', menu3 : 'fuck'}],
-      price1 : 60,
-      price2 : 50,
-      price3 : 40,
+      메뉴들 : ['Home', 'Shop', 'About'],
+      price : ['60', '50', '40'],
+      productsDetail : [
+        {products : '역삼동원룸', price : '60'},
+        {products : '천호동원룸', price : '50'},
+        {products : '마포구원룸', price : '40'}
+      ]
     }
   }
 }
