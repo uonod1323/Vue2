4746757a0ba443773be0dc99f559fb9821a553b0
uonod1323 <uonod1323@naver.com>
2022-12-23 17:29:15 +0900

    뷰스타그램추가

---
+++ vuestagram/src/components/ContainerForm.vue
@@ -0,0 +1,32 @@
+<template>
+  <div>
+    컨테이너임
+    <postList :instaData="instaData[0]"/>
+    <postList :instaData="instaData[1]"/>
+    <postList :instaData="instaData[2]"/>
+  </div>
+</template>
+
+<script>
+import postList from './PostList.vue'
+
+export default {
+  name: 'ContainerForm',
+  props : {
+    instaData : Array,
+    },
+  data(){
+    return{
+
+    }
+  },
+  
+  components: {
+    postList : postList,
+  }
+}
+</script>
+
+<style>
+
+</style>
